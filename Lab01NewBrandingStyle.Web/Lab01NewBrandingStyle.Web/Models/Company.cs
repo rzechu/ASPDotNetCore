34225b6a91c41a0abc3a1235998e308711787656
MRzechowicz <m.rzechowicz@gmail.com>
2020-10-11 23:36:56 +0200

    koniec lab01

---
+++ Lab01NewBrandingStyle.Web/Lab01NewBrandingStyle.Web/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01NewBrandingStyle.Web.Models
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
---
+++ Lab01NewBrandingStyle.Web/Lab01NewBrandingStyle.Web/Models/CompanyAddedViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab01NewBrandingStyle.Web.Models
+{
+    public class CompanyAddedViewModel
+    {
+        public int NumberOfCharsInName { get; set; }
+        public int NumberOfCharsInDescription { get; set; }
+        public bool IsHidden { get; set; }
+    }
+}
