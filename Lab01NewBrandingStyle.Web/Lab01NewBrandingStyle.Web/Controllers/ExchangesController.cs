35c2c17dcc93183626c930dac8c3ee4f0de5c28f
MRzechowicz <m.rzechowicz@gmail.com>
2020-10-11 12:52:05 +0200

    initial commit 1-wsze zajęcia

---
+++ Lab01NewBrandingStyle.Web/Lab01NewBrandingStyle.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab01NewBrandingStyle.Web.Controllers
+{
+    public class ExchangesController : Controller
+    { 
+        public IActionResult Display()
+        {
+            return View();
+        }
+    }
+}
